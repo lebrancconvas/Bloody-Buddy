796333562222f0844b619cc653af474bfee9a690
lebrancconvas <lebrancconvas@gmail.com>
2022-04-10 22:34:42 +0700

    🎨 Change Format.

---
+++ src/config/GameConfig.ts
@@ -0,0 +1,14 @@
+import HelloWorldScene from '../scenes/HelloWorldScene';
+
+export const GameConfig: Phaser.Types.Core.GameConfig = {
+	type: Phaser.AUTO,
+	width: 800,
+	height: 600,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 200 }
+		}
+	},
+	scene: [HelloWorldScene]
+}
--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,6 @@ import Phaser from 'phaser'
 
 import HelloWorldScene from './scenes/HelloWorldScene'
 
-const config: Phaser.Types.Core.GameConfig = {
-	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 }
-		}
-	},
-	scene: [HelloWorldScene]
-}
+import {GameConfig} from './config/GameConfig';
 
-export default new Phaser.Game(config)
+export default new Phaser.Game(GameConfig);
